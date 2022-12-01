fbe8207624ded85dd602e13b649bb40797aa5819
Chou Po Yin <94157717+bobyin22@users.noreply.github.com>
2022-12-01 15:09:58 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+// 初始變數
+let list = document.querySelector('#my-todo')
+let addBtn = document.querySelector('#add-btn')
+let input = document.querySelector('#new-todo')
+
+// 資料
+const todos = ['華固天鑄', '松濤苑', 'Dimond Tower', '陶朱隱園', '潤泰敦峰']
+for (let todo of todos) {
+  addItem(todo)
+}
+
+
+// 函式
+function addItem (text) {
+  let newItem = document.createElement('li')
+  newItem.innerHTML = `
+    <label for="todo">${text}</label>
+    <i class="delete fa fa-trash"></i>
+  `
+  list.appendChild(newItem)
+}
+
+
+// write your code here
+// 1. add new todo
+addBtn.addEventListener('click', function() {
+  let inputValue = input.value;
+  
+  if(inputValue.length > 0){
+    addItem(inputValue)
+  }
+})
+
+// 2.刪除 todo
+list.addEventListener('click', function(event) {
+ let target = event.target
+ 
+ if(target.classList.contains('delete')) {
+   let parentElement = target.parentElement
+   parentElement.remove()
+ }
+  
+  if(target.tagName === 'LABEL') {
+    target.classList.toggle('checked')
+  }
+  
+})
